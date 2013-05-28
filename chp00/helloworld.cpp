80b0018680a6e46976cedc296870e867e0c66467
Yeong Sheng <yeongsheng.tan@baml.com>
2013-05-28 11:01:40 +0800

    Chapter 00

---
+++ chp00/helloworld.cpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  28/05/2013 10:09:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
